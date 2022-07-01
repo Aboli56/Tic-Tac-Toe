9a87fe7ea74a7e9cf6e5f414132c1cd1fb4b805e
Aboli Wankhade <104322603+Aboli56@users.noreply.github.com>
2022-07-01 18:51:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 let win = new Audio("winnn.mp3")
 let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("endd.mp3")
+let gameover = new Audio("go.mpeg")
 let turn = "X";
 let gameOver = false;
 const changeTurn = () => {
@@ -31,15 +31,22 @@ const checkWin = () =>{
      }
     })
 }
+let count=0;
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
 let boxtext = element.querySelector(".boxtext");
 element.addEventListener("click",()=>{
     if(boxtext.innerText === ''){
+        count++;
         boxtext.innerText = turn;
         turn = changeTurn();
         audioTurn.play();
         checkWin();
+         if(count==9){
+            checkWin()
+            gameover.play();
+            alert("Click on Reset button to play again")
+        }
         if(!gameOver){
         document.getElementsByClassName("info")[0].innerText = "Turn for "+turn;
         } 
@@ -53,8 +60,14 @@ reset.addEventListener('click',()=>{
         element.innerText=''
     });
     turn ='X';
+    count=0;
     gameOver=false;
         document.getElementsByClassName("info")[0].innerText = "Turn for "+turn;
         document.querySelector('.gif').getElementsByTagName('img')[0].style.width = "0px"
+     if(count==9){
+            checkWin()
+            gameover.play();
+            alert("Click on Reset button to play again")
+        }
         
 })
